5ac69c24ba64c4cac6649971bf9222b42159c637
alexzone230696-max <alexzone230696@gmail.com>
2025-11-20 09:55:35 +0500

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,5 @@
 <script setup>
-import { init } from '@telegram-apps/sdk'
 import { onMounted } from 'vue'
-import { backButton } from '@telegram-apps/sdk'
-
-onMounted(() => {
-  try {
-    init()
-    if (backButton.isSupported()) {
-      backButton.mount()
-    }
-  } catch (error) {
-    console.log('没有运行在Telegram内')
-  }
-})
 </script>
 
 <template>
